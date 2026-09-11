95c8782682fc79c44e58c84d598f38a5ebf346ef
Grace Currier-Moritsugu <gcurrier-moritsugu@myseneca.ca>
2026-09-11 12:05:27 -0400

    i edited prg.cpp not a0.cpp

--- a0/a0.cpp
+++ a0/a0.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 auto main() -> int {
     std::string subject, name, number, email, repo;
-    std::cout << "### Assignment Zero ###" << std::endl
+    std::cout << "#### Assignment Zero ####" << std::endl
         << "Subject and Section:" << std::endl;
     std::cin >> subject;
-    std::cout << subject << std::endl
+    std::cout
         << "Name:" << std::endl;
     std::cin >> name;
-    std::cout << name << std::endl
+    std::cout
         << "Student Number:" << std::endl;
     std::cin >> number;
-    std::cout << number << std::endl
+    std::cout
         << "Email:" << std::endl;
     std::cin >> email;
-    std::cout << email << std::endl
+    std::cout
         << "Github repository:" << std::endl;
     std::cin >> repo;
-    std::cout << repo << std::endl;
     return 0;
 }
