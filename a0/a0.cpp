0024631d71b6f4f521a17c3b2b57a8e938aa5c92
Grace Currier-Moritsugu <gcurrier-moritsugu@myseneca.ca>
2026-09-11 12:09:17 -0400

    was it overengineered

--- a0/a0.cpp
+++ a0/a0.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 auto main() -> int {
     std::string subject, name, number, email, repo;
-    std::cout << "#### Assignment Zero ####" << std::endl
-        << "Subject and Section:" << std::endl;
-    std::cin >> subject;
-    std::cout
-        << "Name:" << std::endl;
-    std::cin >> name;
-    std::cout
-        << "Student Number:" << std::endl;
-    std::cin >> number;
-    std::cout
-        << "Email:" << std::endl;
-    std::cin >> email;
-    std::cout
-        << "Github repository:" << std::endl;
-    std::cin >> repo;
+    std::cout << "#### Workshop Zero ####" << std::endl
+        << "Subject and Section:" << std::endl
+        << "OOP244 NAA"
+        << "Name:" << std::endl
+        << "Grace Currier-Moritsugu"
+        << "Student Number:" << std::endl
+        << "136335247" << std::endl 
+        << "Email:" << std::endl
+        << "gcurrier-moritsugu@myseneca.ca" << std::endl
+        << "Github repository:" << std::endl
+        << "https://github.com/graceluca/OOP345";
     return 0;
 }
