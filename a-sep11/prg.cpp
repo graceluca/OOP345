dae35570a46fae184dc7365355cee970fc4b2082
Grace Currier-Moritsugu <gcurrier-moritsugu@myseneca.ca>
2026-09-11 11:39:01 -0400

    working on AS00

--- a-sep11/prg.cpp
+++ a-sep11/prg.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 auto main() -> int {
-    std::cout << "Welcome to oop345" << std::endl;
+    std::cout << "### Assignment Zero ###" << std::endl
+        << "Subject and Section:" << std::endl
+        << "OOP244NAA" << std::endl
+        << "Name:" << std::endl
+        << "Grace Currier-Moritsugu" << std::endl
+        << "Student Number:" << std::endl
+        << "136335247" << std::endl
+        << "Email:" << std::endl
+        << "gcurrier-moritsugu@myseneca.ca" << std::endl
+        << "Github repository:" << std::endl
+        << "https://github.com/graceluca/OOP345" << std::endl;
     return 0;
 }
